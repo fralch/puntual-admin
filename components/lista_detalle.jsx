7ddc84946df85a06ac4034ce4fa75536532a48c6
Frank Cairampoma <ingfralch@gmail.com>
2024-01-23 15:44:09 -0500

    actualizar horarios

--- components/lista_detalle.jsx
+++ components/lista_detalle.jsx
@@ -7,6 +7,7 @@ import axios from 'axios';
 function ListaPersonalDetalle(props) {
     const [isEnabled, setIsEnabled] = useState(false);
     const [editable, setEditable] = useState(false);
+    const [actualizarHorario, setActualizarHorario] = useState(false);
     const [modalVisible, setModalVisible] = useState(false);
     const [popupVisible, setPopupVisible] = useState(false);
     const [popupData, setPopupData] = useState({
@@ -15,9 +16,9 @@ function ListaPersonalDetalle(props) {
     });
     const [backHome, setBackHome] = useState(false);
     const [horario, setHorario] = useState({
-        ingreso_manana: '',
+        entrada_manana: '',
         salida_manana: '',
-        ingreso_tarde: '',
+        entrada_tarde: '',
         salida_tarde: '',
     });
 
@@ -42,7 +43,6 @@ function ListaPersonalDetalle(props) {
     useEffect(() => {
         if (id == 0) {
             setEditable(true);
-
         }else{
             axios.get(`http://192.168.1.50:3000/usuarios/${id}`)
             .then(function (response) {
@@ -51,6 +51,26 @@ function ListaPersonalDetalle(props) {
             .catch(function (error) {
                 console.log(error);
             });
+
+            
+        }
+        
+    }, []);
+    useEffect(() => {
+        if (id == 0) {
+            setEditable(true);
+        }else{
+            axios.get(`http://192.168.1.50:3000/horariosLaborales/${id}`)
+            .then(function (response) {
+                console.log(response.data);
+                setHorario(response.data);
+                setActualizarHorario(true);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+            
         }
         
     }, []);
@@ -111,9 +131,91 @@ function ListaPersonalDetalle(props) {
     }
 
     const sendHorarios = async () => {
+        const formatoHoraRegex = /^([0-1]?[0-9]|2[0-3]):([0-5]\d)$/;
         try {
+            for (const key in horario) {
+                if (!formatoHoraRegex.test(horario[key])) {
+                    setPopupData({
+                        titulo: 'Error',
+                        mensaje: 'El formato de hora es incorrecto',
+                    });
+                    setPopupVisible(true);
+                    return;
+                }
+            }
+            const res = await axios.post('http://192.168.1.50:3000/horariosLaborales', {
+                usuario_id: personal.id,
+                entrada_manana: horario.entrada_manana,
+                salida_manana: horario.salida_manana,
+                entrada_tarde: horario.entrada_tarde,
+                salida_tarde: horario.salida_tarde,
+            });
+            console.log(res.data);
+            setModalVisible(false);
+            setPopupData({
+                titulo: 'Exito',
+                mensaje: res.data,
+            });
+            setPopupVisible(true);
+
+            
+
+            
+            
         } catch (error) {
-            console.log(error.response.data);
+            console.log(error.response.data.message);
+            if(error.response.status == 400){
+                setActualizarHorario(true);
+                setPopupData({
+                    titulo: 'Error',
+                    mensaje: error.response.data.message,
+                });
+                setPopupVisible(true);
+                setModalVisible(false);
+
+            }
+        }
+    }
+
+    const sendActualizarHorarios = async () => {
+        const formatoHoraRegex = /^([0-1]?[0-9]|2[0-3]):([0-5]\d):([0-5]\d)$/ ;
+        try {
+            for (const key in horario) {
+                if (!formatoHoraRegex.test(horario[key])) {
+                    setPopupData({
+                        titulo: 'Error',
+                        mensaje: 'El formato de hora es incorrecto',
+                    });
+                    setPopupVisible(true);
+                    return;
+                }
+            }
+            const res = await axios.put('http://192.168.1.50:3000/horariosLaborales', {
+                usuario_id: personal.id,
+                entrada_manana: horario.entrada_manana,
+                salida_manana: horario.salida_manana,
+                entrada_tarde: horario.entrada_tarde,
+                salida_tarde: horario.salida_tarde,
+            
+            });
+            console.log(res.data);
+            setModalVisible(false);
+            setPopupData({
+                titulo: 'Exito',
+                mensaje: res.data,
+            });
+            setPopupVisible(true);
+
+        } catch (error) {
+            console.log(error.response.data.message);
+            setPopupData({
+                titulo: 'Error',
+                mensaje: error.response.data.message,
+            });
+            setPopupVisible(true);
+            setModalVisible(false);
+
+           
         }
     }
 
@@ -193,13 +295,25 @@ function ListaPersonalDetalle(props) {
                         </TextInput>
                     </View>
 
+                    {
+                        !actualizarHorario ?
+                        <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20, backgroundColor: '#EA4D4A', padding: 10, borderRadius: 5 }}
+                            onPress={() => setModalVisible(true)}
+                        >
+                            <Text style={{ color: '#fff', fontSize: 16, fontWeight: 'bold', marginRight: 10 }}>Horarios</Text>
+                            <Feather name="clock" size={24} color="white" />
+                        </TouchableOpacity>
+                        :
+                        <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20, backgroundColor: '#EA4D4A', padding: 10, borderRadius: 5 }}
+                            onPress={() => setModalVisible(true)}
+                        >
+                            <Text style={{ color: '#fff', fontSize: 16, fontWeight: 'bold', marginRight: 10 }}>Actualizar</Text>
+                            <Feather name="clock" size={24} color="white" />
+                        </TouchableOpacity>
+                        
+                    }
 
-                    <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20, backgroundColor: '#EA4D4A', padding: 10, borderRadius: 5 }}
-                        onPress={() => setModalVisible(true)}
-                    >
-                        <Text style={{ color: '#fff', fontSize: 16, fontWeight: 'bold', marginRight: 10 }}>Horarios</Text>
-                        <Feather name="clock" size={24} color="white" />
-                    </TouchableOpacity>
+                   
 
 
                     {editable ?
@@ -213,13 +327,15 @@ function ListaPersonalDetalle(props) {
                             <Feather name="save" size={24} color="white" />
                         </TouchableOpacity>
                         :
-                        <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20, backgroundColor: '#EA4D4A', padding: 10, borderRadius: 5 }}
+                       <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', marginBottom: 20, backgroundColor: '#EA4D4A', padding: 10, borderRadius: 5 }}
                             onPress={() => {
                                 setEditable(true);
-                             }}>
+                                }}>
                             <Text style={{ color: '#fff', fontSize: 16, fontWeight: 'bold', marginRight: 10 }}>Editar</Text>
                             <Feather name="edit" size={24} color="white" />
                         </TouchableOpacity>
+                        
+
                     }
 
                     <Modal animationType="slide" transparent={true} visible={modalVisible} >
@@ -228,29 +344,35 @@ function ListaPersonalDetalle(props) {
                                 <Text style={{ color: '#fff', fontSize: 20, fontWeight: 'bold' }}>Horario laboral</Text>
                                 <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: 10, marginTop: 20 }}>
                                     <TextInput style={{ backgroundColor: '#3d3b3a', borderRadius: 5, padding: 5, marginHorizontal: 10, height: 40, width: 200, color: '#fff' }} placeholder="Ingreso mañanas" placeholderTextColor="gray" 
-                                    onChangeText={(text) => setHorario({ ...horario, ingreso_manana: text })}
+                                     value={horario.entrada_manana}
+                                    onChangeText={(text) => setHorario({ ...horario, entrada_manana: text })}
                                     />
                                 </View>
                                 <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: 10 }}>
                                     <TextInput style={{ backgroundColor: '#3d3b3a', borderRadius: 5, padding: 5, marginHorizontal: 10, height: 40, width: 200, color: '#fff' }} placeholder="Salida mañanas" placeholderTextColor="gray" 
+                                      value={horario.salida_manana}
                                     onChangeText={(text) => setHorario({ ...horario, salida_manana: text })}
                                     />
                                 </View>
                                 <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: 10 }}>
                                     <TextInput style={{ backgroundColor: '#3d3b3a', borderRadius: 5, padding: 5, marginHorizontal: 10, height: 40, width: 200, color: '#fff' }} placeholder="Ingreso tardes" placeholderTextColor="gray"
-                                    onChangeText={(text) => setHorario({ ...horario, ingreso_tarde: text })}
+                                        value={horario.entrada_tarde}
+                                    onChangeText={(text) => setHorario({ ...horario, entrada_tarde: text })}
                                      />
                                 </View>
                                 <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: 10 }}>
                                     <TextInput style={{ backgroundColor: '#3d3b3a', borderRadius: 5, padding: 5, marginHorizontal: 10, height: 40, width: 200, color: '#fff' }} placeholder="Salida tardes" placeholderTextColor="gray" 
+                                        value={horario.salida_tarde}
                                     onChangeText={(text) => setHorario({ ...horario, salida_tarde: text })}
                                     />
                                 </View>
                                 <View style={styles.sugerenciasContainer}>
-
-
                                     <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                                        <TouchableOpacity style={{ ...styles.openButton, backgroundColor: "#EA4D4A", marginBottom: 10 }}>
+                                        <TouchableOpacity style={{ ...styles.openButton, backgroundColor: "#EA4D4A", marginBottom: 10 }}
+                                            onPress={() => {
+                                                actualizarHorario ? sendActualizarHorarios() : sendHorarios();
+                                            }}
+                                        >
                                             <Text style={{ color: 'white' }}>Guardar</Text>
                                         </TouchableOpacity>
                                         <TouchableOpacity style={{ ...styles.openButton, backgroundColor: "#222" }}
